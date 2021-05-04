0b91b74b7655428520652859f68238835621fca1
Deniz Cakan <deniz.cakan@internetx.com>
2021-05-04 22:24:12 +0200

    cleanup

--- internal/telegram/menu.go
+++ internal/telegram/menu.go
@@ -1,6 +1,7 @@
 package telegram
 
 import (
+	"github.com/gohumble/crypto-news-bot/internal/config"
 	"github.com/gohumble/crypto-news-bot/internal/news"
 	"github.com/gohumble/crypto-news-bot/internal/storage"
 	log "github.com/sirupsen/logrus"
@@ -16,10 +17,7 @@ func init() {
 }
 
 var (
-
-	// Universal markup builders.
-	menu = &tb.ReplyMarkup{ResizeReplyKeyboard: true}
-	// Reply buttons.
+	menu          = &tb.ReplyMarkup{ResizeReplyKeyboard: true}
 	btnHelp       = menu.Text("/help")
 	btnSubscribe  = menu.Text("/subscribe")
 	btnGit        = menu.URL("/github", "https://github.com/gohumble/crypto-news-bot")
@@ -27,13 +25,12 @@ var (
 )
 
 func InitHandler(bot *tb.Bot, db *storage.DB, newsfeed *news.Analyzer) {
-	// Command: /start <PAYLOAD>
 	bot.Handle("/start", func(m *tb.Message) {
 		if !m.Private() {
 			return
 		}
 		_, err := bot.Send(m.Sender, markdownEscape("HI! I am a *Crypto News Stream Bot*. \n\n"+
-			"I will fetch the latest news from RSS feeds and categorize and analyze them by coin.\n"+
+			"I will fetch the latest news from RSS feeds and categorize them grouped by coin.\n"+
 			"You just need to subscribe to coins using the /subscribe command.\n"+
 			"News updates will be broadcast to subscribers every hour.\n\n"+
 			"I will scrape the top 100 crypto sites and send you the latest news on subscribed coins by default.\n"+
@@ -61,8 +58,6 @@ func InitHandler(bot *tb.Bot, db *storage.DB, newsfeed *news.Analyzer) {
 						return
 					}
 				}
-				// case 2 -- feed already exists. user subscribes to existing feed!
-
 			}
 
 			if db.Set(user) != nil {
@@ -71,21 +66,21 @@ func InitHandler(bot *tb.Bot, db *storage.DB, newsfeed *news.Analyzer) {
 		}
 	})
 	bot.Handle(&btnGit, func(m *tb.Message) {
-		bot.Send(m.Sender, "https://github.com/gohumble/crypto-news-bot")
+		config.IgnoreErrorMultiReturn(bot.Send(m.Sender, "https://github.com/gohumble/crypto-news-bot"))
 	})
-	// On reply button pressed (message)
+
 	bot.Handle(&btnHelp, func(m *tb.Message) {
-		bot.Send(m.Sender, "Usage:\n\n"+
+		config.IgnoreErrorMultiReturn(bot.Send(m.Sender, "Usage:\n\n"+
 			"/subscribe - subscribe to coins\n"+
 			"/feed - manage your RSS feeds \n"+
 			"/news - get latest news for a single coin\n"+
-			"/sentiments - get sentiment analysis based on all news for all subscribable coins.\n")
+			"/sentiments - get sentiment analysis based on all news for all subscribable coins.\n"))
 	})
 
 	bot.Handle(&btnSentiments, func(m *tb.Message) {
-		bot.Send(m.Sender, newsfeed.GetSentimentTable(), &tb.SendOptions{
+		config.IgnoreErrorMultiReturn(bot.Send(m.Sender, newsfeed.GetSentimentTable(), &tb.SendOptions{
 			ParseMode: tb.ModeMarkdown,
-		})
+		}))
 	})
 	initNewsHandler(bot, db, newsfeed)
 	initSubscriptionHandler(bot, db)
